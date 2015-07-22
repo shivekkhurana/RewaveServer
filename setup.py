1e6e053f96fbfe698e97be43ba87061de4ef1c90
Shivek Khurana <khuranashivek@gmail.com>
2015-07-22 14:51:57 +0530

    py2exe is out, cx freeze is in, need to test

--- setup.py
+++ setup.py
@@ -1,37 +1,43 @@
 
 import sys
+import platform
+from cx_Freeze import setup, Executable
+
+target_dir = "./build/"
+base = None
 
 if sys.platform == 'win32':
-    from distutils.core import setup
-    import py2exe
-    setup(
-    	data_files = [('images', ['images/rewave_app_icon.png'])],
-    	windows = [{
-    		"script":"rewave_server.pyw",
-    		"icon_resources" : [(1, "images/rewave_app_icon.ico")],
-    		"dest_base" : "Rewave Server"
-    	}],
-    	options = {
-    		"py2exe" : {
-    			"optimize" : 2,
-    			"compressed" : 1
-    		}
-    	}
+    base = 'Win32GUI'
+
+if platform.architecture()[0] == '32bit':
+    target_dir = "./buildX86/"
+
+build_options = {
+    "include_files" : ['images'],
+    "optimize": True,
+    "compressed": True,
+    "build_exe": target_dir,
+    "icon": "images/rewave_app_icon.ico"
+}
+
+build_msi_options = {
+    "upgrade_code": "{44B162E5-A1B5-47C3-851D-98A1ADCCE4E0}"
+}
+
+setup(
+    data_files = [('images', ['images/rewave_app_icon.png'])],
+    name = "Rewave Server",
+    version = "0.0.1",
+    description = "Rewave App's Server",
+    executables = [
+        Executable(
+            "rewave_server.pyw", 
+            base=base,
+            targetDir=target_dir
+        )
+    ],
+    options = dict(
+        build_exe = build_options
+        build_msi = build_msi_options
     )
-
-if sys.platform in ['darwin', 'linux']:
-    from cx_Freeze import setup, Executable
-
-    build_options = {
-        "include_files" : ['images']
-    }
-
-    setup(
-        data_files = [('images', ['images/rewave_app_icon.png'])],
-        name = "Rewave Server",
-        version = "0.0.1",
-        description = "Rewave App's Server",
-        executables = [Executable("rewave_server.pyw")],
-        options = dict(build_exe = build_options),
-        icon="images/rewave_app_icon.ico"
-    )
+)
