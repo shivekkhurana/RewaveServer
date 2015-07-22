2fc0de9c5ee11f859aba5359dd35e29b6149c48e
Shivek Khurana <shivekkhurana@du>
2015-07-22 15:08:02 +0530

    cxFreeze errored out with windows. Reverting to py2exe + cxFreeze

--- setup.py
+++ setup.py
@@ -1,43 +1,50 @@
 
 import sys
-import platform
-from cx_Freeze import setup, Executable
 
-target_dir = "./build/"
-base = None
-
+##### WINDOWS ######
 if sys.platform == 'win32':
-    base = 'Win32GUI'
-
-if platform.architecture()[0] == '32bit':
-    target_dir = "./buildX86/"
-
-build_options = {
-    "include_files" : ['images'],
-    "optimize": True,
-    "compressed": True,
-    "build_exe": target_dir,
-    "icon": "images/rewave_app_icon.ico"
-}
-
-build_msi_options = {
-    "upgrade_code": "{44B162E5-A1B5-47C3-851D-98A1ADCCE4E0}"
-}
-
-setup(
-    data_files = [('images', ['images/rewave_app_icon.png'])],
-    name = "Rewave Server",
-    version = "0.0.1",
-    description = "Rewave App's Server",
-    executables = [
-        Executable(
-            "rewave_server.pyw", 
-            base=base,
-            targetDir=target_dir
-        )
-    ],
-    options = dict(
-        build_exe = build_options
-        build_msi = build_msi_options
+    from distutils.core import setup
+    import py2exe
+    setup(
+    	data_files = [('images', ['images/rewave_app_icon.png'])],
+    	windows = [{
+    		"script":"rewave_server.pyw",
+    		"icon_resources" : [(1, "images/rewave_app_icon.ico")],
+    		"dest_base" : "Rewave Server"
+    	}],
+    	options = {
+    		"py2exe" : {
+    			"optimize" : 2,
+    			"compressed" : 1
+    		}
+    	}
     )
-)
+
+
+##### MAC & Ubuntu ######
+if sys.platform in ['darwin', 'linux']:
+	from cx_Freeze import setup, Executable
+
+	build_options = {
+	    "include_files" : ['images'],
+	    "optimize": True,
+	    "compressed": True,
+	    "build_exe": target_dir,
+	    "icon": "images/rewave_app_icon.ico"
+	}
+
+	setup(
+	    data_files = [('images', ['images/rewave_app_icon.png'])],
+	    name = "Rewave Server",
+	    version = "0.0.1",
+	    description = "Rewave App's Server",
+	    executables = [
+	        Executable(
+	            "rewave_server.pyw", 
+	            base=base,
+			)
+	    ],
+	    options = dict(
+	        build_exe = build_options
+	    )
+	)
